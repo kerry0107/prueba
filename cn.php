1d58b6a1515502d77559d1cac10a4215b421db56
kerry0107 <65262562+kerry0107@users.noreply.github.com>
2020-05-13 00:55:46 -0400

    Add files via upload

---
+++ cn.php
@@ -0,0 +1,11 @@
+<?php
+
+$conexion = @mysqli_connect("sql10.freesqldatabase.com", "sql10340121", "bDaTIs9ieG", "sql10340121");
+//if (!$conexion){
+ //   echo 'Error al conectar a la base de datos';
+//}
+//else {
+  //  echo 'Conectado a la base de datos';
+//}
+
+?>
